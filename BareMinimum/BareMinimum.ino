7f6734158d572ff3efe5ee21512d018f3b01b429
jeff2234345 <jefferson.valdezrodriguez@dawsoncollege.qc.ca>
2025-04-14 10:03:19 -0400

    Changed message on LCD

--- BareMinimum/BareMinimum.ino
+++ BareMinimum/BareMinimum.ino
@@ -17,7 +17,7 @@ void setup() {
 
 void loop() {
   lcd.clear();                  // Clear the LCD screen
-  lcd.print("Press to Start");  // Display instruction to the player
+  lcd.print("Press to begin");  // Display instruction to the player
 
   // Just waiting here until the player presses the button (HIGH to LOW)
   while (digitalRead(buttonPin) == HIGH) {}  // DONT WORRY HOW THIS WORKS NOW
